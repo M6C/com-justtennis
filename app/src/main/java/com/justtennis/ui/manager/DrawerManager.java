a452571d15ea5a6bb8e65f8b25abe16924955bb9
David ROCA <david.roca@laposte.fr>
2018-11-16 10:06:28 +0100

    Fix ClassCastException DrawerLayout

--- app/src/main/java/com/justtennis/ui/manager/DrawerManager.java
+++ app/src/main/java/com/justtennis/ui/manager/DrawerManager.java
@@ -4,6 +4,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.justtennis.R;
 
@@ -13,10 +14,11 @@ public class DrawerManager {
     }
 
     public void initializeDrawerToogle(FragmentActivity activity) {
-        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.detail_toolbar);
-        DrawerLayout drawerLayout = (DrawerLayout) activity.findViewById(R.id.drawer_layout);
+        Toolbar toolbar = activity.findViewById(R.id.detail_toolbar);
+        View layout = activity.findViewById(R.id.drawer_layout);
 
-        if (toolbar != null && drawerLayout != null) {
+        if (toolbar != null && layout instanceof DrawerLayout) {
+            DrawerLayout drawerLayout = (DrawerLayout)layout;
             ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(activity, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
             drawerLayout.addDrawerListener(toggle);
             toggle.syncState();
